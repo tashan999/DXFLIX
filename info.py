dbe8c3ae87bd9cb5b6b5657b52c0888d6efbc028
tashan999 <138196222+tashan999@users.noreply.github.com>
2026-09-11 13:51:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -78,13 +78,13 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tashanrp:tashanrp@databasesec.5fk0zb6.mongodb.net/?retryWrites=true&w=majority&appName=DatabaseSec")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "akshat") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tzcollection2') # Collection name (default: dreamcinezone_files)
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), True) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://codexflix:codexflix@cluster0.alkmsk4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
+DATABASE_URI2 = environ.get('DATABASE_URI2', "")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
 # ============================
 # Movie Notification & Update Settings
 # ============================
